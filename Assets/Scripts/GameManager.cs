1a52dd0843e7f49506886278bd3f9253e38d1ee7
Ethan McGimpsey <54158496+MrMuskyHusky@users.noreply.github.com>
2019-11-08 15:24:46 +1100

    Update #11 (Finish)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
+    #region Singleton
+    public static GameManager Instance = null;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    public int score = 0; // ScoreKeeping
+
+    public void AddScore(int scoretoAdd)
+    {
+        // Increase Score Value by incoming score
+        score += scoretoAdd;
+        // Update UI Here
+        UIManager.Instance.UpdateScore(score);
+    }
+
     public void NextScene()
     {
         Scene activeScene = SceneManager.GetActiveScene();
---
+++ Assets/Scripts/Other Scripts/Characters/Player.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class Player : MonoBehaviour
+{
+    // Member 
+    public float jumpHeight = 5f; // How high the Character Jumps (in units)
+    public float climbSpeed = 10f; // How fast the Character Climbs
+    public float moveSpeed = 10f; // How fast the Character Moves
+    public float portalDistance = 1f; // Hpw far from the Portal the player needs to be to press Up and Interact
+
+    private CharacterController2D controller;
+    private Transform currentPortal; // Reference to Current Portal
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Gather components at the start of the game to save processing! (Cache-ing)
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    void OnDrawGizmos()
+    {
+        // If player is over a portal (Trigger)
+        if(currentPortal != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(currentPortal.position, portalDistance);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         * --- Unity Tip ---
+         * Input.GetAxis - 
+         * Input.GetAxisRaw -
+         */
+
+        // Temporary Variables
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        bool isJumping = Input.GetButtonDown("Jump");
+
+        if(isJumping)
+        {
+            controller.Jump(jumpHeight);
+        }
+
+        controller.Climb(vertical * climbSpeed);
+
+        // Move controller horizontally
+        controller.Move(horizontal * moveSpeed);
+
+        // If over a portal
+        if(currentPortal != null)
+        {
+            // If Up Arrow is pressed once this frame
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                // Get distance between player and portal
+                float distance = Vector2.Distance(transform.position, currentPortal.position);
+                // If Distance is Less than PortalDistance
+                if (distance < portalDistance)
+                {
+                    // Fire off Interact Function
+                    currentPortal.SendMessage("Interact");
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // Detect hitting item
+        if(col.gameObject.tag == "Item")
+        {
+            // Add 1 to score - Requires a GameManager
+            GameManager.Instance.AddScore(1);
+            // Play chim sound - Requires an Audio Source
+            Destroy(col.gameObject);
+        }
+        // Detect hitting portal
+        if (col.CompareTag("Portal"))
+        {
+            // Store the current portal
+            currentPortal = col.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider2D col)
+    {
+        // Detect hitting protal
+        if (col.CompareTag("Portal"))
+        {
+            // Store the current protal
+            currentPortal = null;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        // Detect hitting portal
+        if (col.CompareTag("Portal"))
+        {
+            // Check if Up is pressed
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                // Invoke Action Event on the Portal using 'SendMessage'
+                col.SendMessage("Interact");
+            }
+        }
+    }
+}
